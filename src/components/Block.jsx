b5581797b46ccbf122a978d5ddd50a2d20d27c78
sajal90 <sajal2005gupta@gmail.com>
2025-11-16 23:14:04 +0530

    hover effect

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,19 +1,24 @@
+import { useState } from "react"
+
 const Block = ({covered, bomb, handleClick, rowIdx, colIdx, val}) => {
+    const [hover, setHover] = useState(false)
+
     const style = {
-        backgroundColor: !covered&&!bomb ? "#484848ff" : "white",
+        backgroundColor: !covered&&!bomb ? "#484848ff" : hover ? "#9cfb03ff" : "white",
         width: "55px",
         height: "55px",
         borderRadius: "10px",
         border: "1px solid black",
         color: "#f20000ff",
         fontWeight: "bold",
-        fontSize: "40px"
+        fontSize: "40px",
     }
 
     if(!covered && !bomb && val) {
         return (
             <div>
-                <button style={style} type="button" onClick={() => handleClick(rowIdx, colIdx)}>
+                <button onMouseOver={()=>setHover(true)}  onMouseLeave={() => setHover(false)}
+                    style={style} type="button" onClick={() => handleClick(rowIdx, colIdx)}>
                     {val}
                 </button>
             </div>
@@ -21,7 +26,8 @@ const Block = ({covered, bomb, handleClick, rowIdx, colIdx, val}) => {
     }
     return (
         <div>
-            <button style={style} type="button" onClick={() => handleClick(rowIdx, colIdx)}></button>
+            <button onMouseOver={()=>setHover(true)} onMouseLeave={() => setHover(false)}
+             style={style} type="button" onClick={() => handleClick(rowIdx, colIdx)}></button>
         </div>
     )
 }
